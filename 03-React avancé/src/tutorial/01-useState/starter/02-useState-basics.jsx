4ade5a7ae86d9290b99b503667b1b602ac382ece
DayOneAI <bambathiam88@gmail.com>
2023-06-03 01:15:00 +0200

    usestate-basics-end

--- 03-React avancé/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-React avancé/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,26 @@
-const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateBasics;
+const UseStateBasics = () => {
+  // const value = useState('Bamba')[0]
+  // const handler = useState('Bamba')[1]
+  // console.log(value)
+  // console.log(handler)
+
+  let [count, setCount] = useState(0)
+  console.log(typeof count)
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <h2>Tu as cliqué {count} fois</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Incrémente
+      </button>
+    </div>
+  )
+}
+
+export default UseStateBasics
