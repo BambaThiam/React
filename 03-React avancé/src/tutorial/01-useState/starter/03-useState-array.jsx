c41976bbe84062073f908aa5405a1f5b08046289
DayOneAI <bambathiam88@gmail.com>
2023-06-04 00:25:08 +0200

    usestate-array_ex

--- 03-React avancé/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-React avancé/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,35 @@
-const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+import { useState } from 'react'
+import { data } from '../../../data'
 
-export default UseStateArray;
+const UseStateArray = () => {
+  const [people, setPeople] = useState(data)
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+    data.filter(person)
+  }
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person
+        return (
+          <div key={id} className="item">
+            <h2>{name}</h2>
+            <button onClick={() => removeItem(id)}>Remove</button>
+          </div>
+        )
+      })}
+      <button
+        className="btn"
+        style={{ marginTop: '2rem' }}
+        onClick={() => setPeople([])}
+      >
+        clear items
+      </button>
+    </div>
+  )
+}
+
+export default UseStateArray
