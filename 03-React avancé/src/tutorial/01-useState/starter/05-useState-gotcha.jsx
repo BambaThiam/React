bb67aa17f785f8a332bec1da603afe9340699ff3
DayOneAI <bambathiam88@gmail.com>
2023-06-09 23:25:07 +0200

    useEffect_fondam_end

--- 03-React avancé/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-React avancé/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,9 +3,22 @@ import { useState } from 'react'
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0)
 
+  // const handleClick = () => {
+  //   setValue(value + 1)
+  //   console.log(setValue)
+  // }
+
   const handleClick = () => {
-    setValue(value + 1)
-    console.log(setValue)
+    // setTimeout(() => {
+    //   //console.log('Click button')
+    //   setValue(value + 1)
+    // }, 3000)
+    setTimeout(() => {
+      console.log('clicked the button')
+      setValue((currentState) => {
+        return currentState + 1
+      })
+    }, 3000)
   }
   return (
     <div>
