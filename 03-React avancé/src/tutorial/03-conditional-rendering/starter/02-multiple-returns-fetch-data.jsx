800db916b8449f95976ae03e91101310c21afe99
DayOneAI <bambathiam88@gmail.com>
2023-06-10 19:08:56 +0200

    rendu_con_fetch_end

--- 03-React avancé/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-React avancé/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
-  const [isLoading, seIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
   const [isError, setIsError] = useState(false)
   const [user, setUser] = useState(null)
 
@@ -14,17 +14,30 @@ const MultipleReturnsFetchData = () => {
         setUser(user)
         console.log(user)
       } catch (error) {
+        setIsError(true)
         console.log(error)
       }
+      setIsLoading(false)
     }
     fetchUser()
   }, [])
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+  if (isError) {
+    return <h2>Ther wa an error...</h2>
+  }
   return (
-    <section>
-      {
-        // const {avatar_url, name, company, bio} = user
-      }
-    </section>
+    <div>
+      <img
+        style={{ width: '150px', borderRadius: '25px' }}
+        src={user.avatar_url}
+        alt={user.name}
+      />
+      <h2>{user.name}</h2>
+      <h4>works at {user.company}</h4>
+      <p>{user.bio}</p>
+    </div>
   )
 }
 export default MultipleReturnsFetchData
