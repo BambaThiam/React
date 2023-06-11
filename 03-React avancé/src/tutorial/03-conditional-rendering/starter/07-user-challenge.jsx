7ede164893b2c5004a7e99c678cea16cb03181a8
DayOneAI <bambathiam88@gmail.com>
2023-06-12 00:14:02 +0200

    login_logout

--- 03-React avancé/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 03-React avancé/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from 'react'
 
-export default UserChallenge;
+const UserChallenge = () => {
+  const [user, setUser] = useState(null)
+
+  const login = () => {
+    setUser({ name: 'Bamba' })
+  }
+
+  const logout = () => {
+    setUser(null)
+  }
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>Hello there, {user.name}</h4>,
+          <button className="btn" onClick={logout}>
+            logout
+          </button>
+        </div>
+      ) : (
+        ((<h4>Please login</h4>),
+        (
+          <button className="btn" onClick={login}>
+            login
+          </button>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default UserChallenge
